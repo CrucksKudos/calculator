8cee48c44a097c12d90b7b2d1d9c00d95a5e1b1b
CrucksKudos <29085362+CrucksKudos@users.noreply.github.com>
2025-01-31 23:20:28 -0300

    Add operate function

--- script.js
+++ script.js
@@ -14,3 +14,33 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(a, b, operator) {
+    
+    let result = 0;
+
+    switch(operator) {
+        
+        case "add":
+            result = add(a, b);
+            break;
+
+        case "subtract":
+            result = subtract(a, b);
+            break;
+        
+        case "multiply":
+            result = multiply(a, b);
+            break;
+
+        default:
+            result = divide(a, b);
+    }
+
+    return result;
+}
+
+let a = 10
+let b = 15
+let operator = "add"
+
+console.log(operate(a,b,operator))
